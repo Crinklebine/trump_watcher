8761045234e7a8c5b0fe06756bc15a4d6d182bd1
Crinklebine <4390399+Crinklebine@users.noreply.github.com>
2025-05-01 14:57:46 +0700

    Change poll period to 60 seconds

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ USER_AGENT = (
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
     "(KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36 Edg/135.0.0.0"
 )
-POLL_INTERVAL = 30  # seconds between page reloads
+POLL_INTERVAL = 60  # seconds between page reloads
 seen_hashes = set()  # store hashes of posts already notified
 
 # ----------------------------
